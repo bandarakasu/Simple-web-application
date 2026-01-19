a2024413648f4ee943c6034c9e5a21ff7b445501
bandarakasu <kasubsampath@gmail.com>
2026-01-19 14:31:56 +0530

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,9 @@
+# Initialize git if not already done
+git init
+git add .
+git commit -m "Initial commit - Sampath Portfolio with Radio Player"
+
+# Push to GitHub
+git branch -M main
+git remote add origin https://github.com/yourusername/sampath-portfolio.git
+git push -u origin main
